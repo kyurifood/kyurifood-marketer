5f23c125008a8dd09eba2e7eede8d0946e197100
anggi riyandi <anggi.riyandi@gmail.com>
2016-05-16 16:07:56 +0700

    structure folder aplikasi

--- app/src/main/java/com/artivisi/android/kyurifood/marketer/activities/MainActivity.java
+++ app/src/main/java/com/artivisi/android/kyurifood/marketer/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.artivisi.android.kyurifood.marketer;
+package com.artivisi.android.kyurifood.marketer.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.artivisi.android.kyurifood.marketer.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/artivisi/android/kyurifood/marketer/fragments/MainFragment.java
@@ -0,0 +1,7 @@
+package com.artivisi.android.kyurifood.marketer.fragments;
+
+/**
+ * Created by anggi on 16/05/16.
+ */
+public class MainFragment {
+}
